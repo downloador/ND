cb15c79d2a5ff93e96ff3fc8f54d246722ad76d4
downloador <38085070+downloador@users.noreply.github.com>
2021-04-24 20:10:12 +0200

    Update RenderService.lua

--- RenderService.lua
+++ RenderService.lua
@@ -1,11 +1,15 @@
 local Player =  game:GetService("Players").LocalPlayer
 local Character = Player.character or Player.characterAdded:Wait()
 
-game:GetService("RunService").RenderStepped:Connect(function()
+local u1 = game:GetService("RunService").RenderStepped:Connect(function()
 	for i, part in pairs(Character:GetChildren())do
 		if string.match(part.Name, "Left") or string.match(part.Name, "Right") or string.match(part.Name, "Lower") then
 			part.LocalTransparencyModifier = 0
 		end
 	end
 	Character.Humanoid.CameraOffset = Vector3.new(0,0.02,-.05)
+end)
+
+Character.Humanoid.Died:Connect(function()
+	u1:Disconnect()
 end)
